104e4d0d88fb10d39102c7a068b814e78f0fbbd6
Edward Groberski <egroberski@gmail.com>
2015-02-14 09:47:37 -0600

    Created blank Xcode project
    Used single view template and removed view and view controllers.

---
+++ Musick/Musick/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Musick
+//
+//  Created by Eddie Groberski on 2/14/15.
+//  Copyright (c) 2015 Eddie Groberski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
